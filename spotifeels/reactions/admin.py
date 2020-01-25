c457ff459a002ee5afe4049e2d924bb94909b119
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2020-01-25 16:44:48 -0500

    Added Skill, Task and Reaction models

--- spotifeels/reactions/admin.py
+++ spotifeels/reactions/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Reaction
+from .models import Reaction, Skill
 
 # Register your models here.
 admin.site.register(Reaction)
+admin.site.register(Skill)
--- spotifeels/reactions/models.py
+++ spotifeels/reactions/models.py
@@ -1,27 +1,41 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# # Create your models here.
+# Create your models here.
 
-# class Profile(models.Model):
-# 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-# 	bio = models.TextField(max_length=500, blank=True)
-# 	location = models.CharField(max_length=30, blank=True)
-# 	birth_date = models.DateField(null=True, blank=True)
+class Skill(models.Model):
+    user = models.ManyToManyField(User, blank=True)
+    name = models.TextField()
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+    def __str__(self):
+        return f'{self.name}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Task(models.Model):
+    user = models.ManyToManyField(User, blank=True)
+    title = models.TextField()
+    skills = models.ManyToManyField(Skill, blank=True)
 
 class Reaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     mood = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     comment = models.TextField(null=True, blank=True)
-    time_of_reaction = models.TimeField(auto_now_add=True)
+    time_of_reaction = models.DateTimeField(auto_now_add=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.user} was feeling {self.mood} at {self.time_of_reaction}'
