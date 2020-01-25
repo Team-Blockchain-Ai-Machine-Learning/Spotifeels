43edc75730b967ad7cd73487358d0cba8c78959d
sasgilmer <46273273+sasgilmer@users.noreply.github.com>
2020-01-25 17:42:00 -0500

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,23 @@ import HRView from "./HRView";
 import UserView from "./UserView";
 import ManagerView from "./ManagerView";
 import Interface from "./Interface";
+import { render } from 'react-dom';
 
 function App() {
 
   var i = new Interface();
   
-  if (i.getSession() != null) {
+  if (false) {//(i.getSession() != null) {
     return (<Login callback={loadApp} />);
-  } 
+  }
 
   var user = {level: 0};//i.getUser(uid);
 
-  if (user.level = 0) {
+  if (user.level === 0) {
     return(<UserView />);
-  } else if (user.level = 1) {
+  } else if (user.level === 1) {
     return(<ManagerView />);
-  }else if (user.level = 2) {
+  }else if (user.level === 2) {
     return(<HRView />);
   }
   
