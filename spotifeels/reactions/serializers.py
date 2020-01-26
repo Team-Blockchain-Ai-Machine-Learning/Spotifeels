f39abacc52090dab7d4d9d692846f044fa6f8793
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2020-01-25 22:37:42 -0500

    Fixed unsafe serialization of user model

--- spotifeels/reactions/serializers.py
+++ spotifeels/reactions/serializers.py
@@ -11,17 +11,17 @@ class ReactionSerializer(serializers.ModelSerializer):
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        # fields = ('user', 'mood', 'comment')
+        # fields = ('user', 'title', 'skills')
         fields = '__all__'
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        # fields = ('user', 'mood', 'comment')
+        # fields = ('user', 'name')
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        # fields = ('user', 'mood', 'comment')
-        fields = '__all__'
+        # fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ['username', 'first_name', 'last_name', 'email']
