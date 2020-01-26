b02b2b7907601197f184696d5627ce960d78828f
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-25 21:31:09 -0500

    Add basic HR view

--- frontend/src/HRView.jsx
+++ frontend/src/HRView.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import LineChart from './LineChart';
+
+var reactions = [
+    {
+        mood: 50,
+        note: "I hate my life",
+        time: 9
+    },
+    {
+        mood: 100,
+        note: "My boss hit on me",
+        time: 10
+    },
+    {
+        mood: 0,
+        note: "He told me it was a joke",
+        time: 10.1
+    },
+    {
+        mood: 75,
+        note: "Lunch with Jane",
+        time: 12
+    },
+    {
+        mood: 25,
+        note: "So many emails",
+        time: 14
+    },
+    {
+        mood: 90,
+        note: "I get to go home!",
+        time: 17
+    },
+];
 
 export default class HRView extends React.Component {
-  state = { 
-    center: null
-  };
+  constructor(props) {
+    super();
+    var users = [];
+    for (var i = 0; i < 10; i++) {
+      users.push({name: "name"+i, reactions: reactions})
+    }
+    this.state = { 
+      users: users
+    };
+  }
 
   render() {
-    return <h1>Hello</h1>;
+    return(
+      <div>
+        {this.state.users.map((user) => (<LineChart name={user.name} reactions={user.reactions}/>))}
+      </div>
+    );
   }
 }
