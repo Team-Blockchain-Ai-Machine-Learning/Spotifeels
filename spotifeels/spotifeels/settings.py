3b09030f7b47087cfbc77b3a22bbf5f7d497e333
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2020-01-26 07:06:24 -0500

    Rolled back CORS changes

--- spotifeels/spotifeels/settings.py
+++ spotifeels/spotifeels/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
     'reactions',
     'rest_framework',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'spotifeels.urls'
@@ -123,9 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CORS_ORIGIN_ALLOW_ALLCORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'https://localhost:3000'
-)
